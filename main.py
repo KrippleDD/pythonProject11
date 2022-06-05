1559d15ec92a44e3ce80f0f6b710ffef613f56ba
KrippleDD <106098792+KrippleDD@users.noreply.github.com>
2022-06-05 19:37:10 +0300

    Update main.py
    1

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+f = open('input.txt')
+dict = {}
+for line in f:
+    a = map(str, line.split())
+    a = list(a)
+    for i in range(len(a)):
+        if a[i] in dict:
+            dict[a[i]] += 1
+        else:
+            dict[a[i]] = 0
+        print(dict[a[i]])
